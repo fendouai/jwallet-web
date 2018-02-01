7cd485379d30583687060724d563eebf25ee7add
erohin <erohin.em@gmail.com>
2018-02-01 19:08:53 +0300

    Refactor transactions constants

--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -7,8 +7,11 @@ export const SET_SORT_OPTIONS = '@transactions/Set sort options'
 export const SET_SEARCH_OPTIONS = '@transactions/Set search options'
 
 // Maybe like in sort and search, through options - SET_FILTER_OPTIONS?
-export const SET_END_FILTER_TIME = '@transactions/Set end filter time'
-export const SET_START_FILTER_TIME = '@transactions/Set start filter time'
+
+// Rename to SET_FILTER_END_TIME
+export const SET_END_FILTER_TIME = '@transactions/Set filter end time'
+// Rename to SET_FILTER_START_TIME
+export const SET_START_FILTER_TIME = '@transactions/Set start filter start time'
 
 // Maybe it must be SEARCH_REQUEST_ERROR?
 export const SET_BLOCK_EXPLORER_ERROR = '@transactions/Set block explorer error'
