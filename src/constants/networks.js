9b3c41137e53791169e0e34d4fc4617abf2e8427
erohin <erohin.em@gmail.com>
2018-02-01 22:09:22 +0300

    Add networks constants

---
+++ src/constants/networks.js
@@ -0,0 +1,6 @@
+export const GET = '@networks/Get' // Do we need this?
+export const SET = '@networks/Set'
+export const SET_CURRENT = '@networks/Set current' // Maybe SET_CURRENT_NETWORK?
+export const SAVE_CUSTOM_NETWORK = '@networks/Save custom network'
+export const REMOVE_CUSTOM_NETWORK = '@networks/Remove custom network'
+export const SET_CUSTOM_NETWORK_VALUE = '@networks/Set custom network value'
