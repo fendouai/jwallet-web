3d82aa1ccfa8ac4d4743ba2a30e5da6fa1fa2880
erohin <erohin.em@gmail.com>
2018-02-01 19:06:44 +0300

    Refactor currencies reducer

--- src/reducers/currencies.js
+++ src/reducers/currencies.js
@@ -1,11 +1,14 @@
-
-/* eslint-disable arrow-parens, max-len, no-shadow, function-paren-newline */
+/* eslint-disable max-len */
 
 import { identity, always } from 'ramda'
-import * as CURRENCIES from '../../../constants/currencies'
+import * as CURRENCIES from '../constants/currencies'
 import * as helpers from '../helpers'
 
-const { applyPayload, applyPayloadTo, setField, seqModify } = helpers.store
+const {
+  setField,
+  seqModify,
+  applyPayload,
+} = helpers.store
 
 const initialState = {
   items: [],
@@ -28,31 +31,35 @@ const actionsHandlers = {
   [CURRENCIES.SET]:
     applyPayload('items'),
 
-  [CURRENCIES.SET_CURRENT]: seqModify(
-    setField('isLoading', false),
-    applyPayload('currentAddress'),
-  ),
+  [CURRENCIES.SET_CURRENT]:
+    seqModify(
+      setField('isLoading', false),
+      applyPayload('currentAddress'),
+    ),
 
   [CURRENCIES.SET_ACTIVE_ALL]:
     applyPayload('isActiveAll'),
 
-  [CURRENCIES.OPEN_MODAL]: seqModify(
-    setField('isOpen', true),
-    applyPayload('onClose'),
-  ),
+  [CURRENCIES.OPEN_MODAL]:
+    seqModify(
+      setField('isOpen', true),
+      applyPayload('onClose'),
+    ),
 
   [CURRENCIES.CLOSE_MODAL]:
     setField('isOpen', false),
 
-  [CURRENCIES.SET_SEARCH_OPTIONS]: seqModify(
-    applyPayloadTo('searchQuery', 'searchQuery'), // Example if item in action payload and store field not match
-    applyPayload('foundItemsSymbols'),
-  ),
+  [CURRENCIES.SET_SEARCH_OPTIONS]:
+    seqModify(
+      applyPayload('searchQuery'),
+      applyPayload('foundItemsSymbols'),
+    ),
 
-  [CURRENCIES.SET_SORT_OPTIONS]: seqModify(
-    applyPayload('sortField'),
-    applyPayload('sortDirection')
-  ),
+  [CURRENCIES.SET_SORT_OPTIONS]:
+    seqModify(
+      applyPayload('sortField'),
+      applyPayload('sortDirection')
+    ),
 
   [CURRENCIES.SET_BALANCES]:
     applyPayload('balances'),
