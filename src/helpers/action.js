c7766a549ad0a57c0f3bac9324ca0115d643555f
erohin <erohin.em@gmail.com>
2018-02-01 16:48:23 +0300

    Add action helpers

---
+++ src/helpers/action.js
@@ -0,0 +1,9 @@
+import { is, path, curry, concat } from 'ramda'
+
+// Create FSA action
+export const create = (type, payload, meta) => ({ type, payload, meta })
+
+// Get some item from action payload
+export const getPayload = curry((item, action) =>
+  path(is(Array, item) ? concat(['payload'], item) : ['payload', item], action)
+)
---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+import * as reducer from './reducer'
+import * as action from './action'
+import * as store from './store'
+
+export { reducer, action, store }
